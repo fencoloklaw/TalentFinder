f077e82902405335a3e833e49fc268d883e11649
fencoloklaw <fencoloklaw@gmail.com>
2017-10-22 20:56:58 -0700

    update dbpassword

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://fenco:yolo420swag@ds115352.mlab.com:15352/authapp',
+    database: 'mongodb://fenco1:123456@ds115352.mlab.com:15352/authapp',
     secret: 'yoursecret'
 }
