0eb05055c98fcc2a5de00e2867dd061ba86d596a
fencoloklaw <fencoloklaw@gmail.com>
2018-12-21 20:17:36 -0800

    deploy fix

--- lib/routes/users.ts
+++ lib/routes/users.ts
@@ -16,9 +16,9 @@ export class Routes {
             res.send('Invalid Endpoint');
         });
 
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../public/index.html'));
-        });
+        // app.get('*', (req, res) => {
+        //     res.sendFile(path.join(__dirname, '../public/index.html'));
+        // });
 
         app.route('/users/register')
             .post((req: Request, res: Response) => {
